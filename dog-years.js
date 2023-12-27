f1de68afbe5751ee1bd9c1908f436383f53b7129
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2023-12-27 16:05:33 +0100

    Finished Dog Years project

---
+++ dog-years.js
@@ -0,0 +1,20 @@
+// Initialize myAge
+const myAge = 44;
+
+// Initialize earlyYears
+const earlyYears = 2 * 10.5;
+
+// Initialize laterYears
+const laterYears = (myAge - 2) * 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+// Add together earlyYears and laterYears
+const myAgeInDOgYears = earlyYears + laterYears;
+
+// Initialize myName
+const myName = 'Marjolein'.toLowerCase();
+
+// Console.log myName, myAge and myAgeInDOgYears in a sentence
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDOgYears} years old in dog years.`);
